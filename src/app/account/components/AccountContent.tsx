aa245d39a7fd939d18bdde8386b7a80ba9282ea4
ITrunner1 <MatveyT1337@yandex.ru>
2023-11-20 22:10:01 +1000

    Created account page

---
+++ src/app/account/components/AccountContent.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import { useUser } from "@/hooks/useUser";
+
+const AccountContent = () => {
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace('/');
+        }
+    }, [isLoading, user, router]);  
+    
+    return (
+        <div className="mb-7 px-6">
+            <div className="flex flex-col gap-y-4">
+                
+            </div>
+        </div>
+    );
+};
+
+export default AccountContent;
---
+++ src/app/account/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import AccountContent from "./components/AccountContent";
+
+const Account = () => {
+    return (
+        <div className="dark text-foreground min-h-screen min-w-max">
+            <Header>
+                <div  className="mb-2 flex flex-col gap-y-6">
+                    <h1 className="text-mattewhite text-4xl sm:text-5xl lg-7xl font-bold">
+                        Account Settings
+                    </h1>
+                </div>
+            </Header>
+            <AccountContent />
+        </div>
+    );
+}
+
+export default Account;
